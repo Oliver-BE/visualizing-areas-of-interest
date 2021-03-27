063bdc6a8e70140429305665503c1d3298c24997
Isaac Caruso <44649197+icaruso21@users.noreply.github.com>
2021-03-26 18:56:03 -0700

    Update containers.sh

--- srv/containers.sh
+++ srv/containers.sh
@@ -61,5 +61,5 @@ echo "###    Adding update scripts to crontab"
 sudo chmod -x ~/visualizing-areas-of-interest/srv/*.sh
 #sudo cp /srv/refreshServer.sh /etc/cron.hourly/
 sudo chmod -x /srv/shinyapps/visualizing-areas-of-interest/srv/refreshServer.sh
-(crontab -l 2>/dev/null; echo "* * * * * /srv/shinyapps/visualizing-areas-of-interest/srv/refreshServer.sh") | crontab -
+(crontab -l 2>/dev/null; echo "* * * * * sudo bash /srv/shinyapps/visualizing-areas-of-interest/srv/refreshServer.sh") | crontab -
 #(crontab -l 2>/dev/null; echo "00 02 * * * docker run --rm -v /srv/shinyapps/RShiny_BiophysicalModelMap:/code yutaro/updates >> '/home/ec2-user/yutaro_app_updates.log' 2>&1") | crontab -
